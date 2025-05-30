412a10372b51f845d26436de9cbc926d8cb05f4a
consolTzotzis <evangelos.tzotzis@consolut.com>
2025-05-30 09:05:41 +0300

    Update commonUtils.groovy

--- commonUtils.groovy
+++ commonUtils.groovy
@@ -1,23 +1,5 @@
 // commonUtils.groovy
 
-return [
-    analyzeName: { fullName ->
-        def parts = fullName.trim().split(/\s+/)
-        def firstName = parts[0]
-        def lastName = parts[-1]
-        def initials = parts.collect { it[0].toUpperCase() }.join(".") + "."
-        def reversed = parts.reverse().join(" ")
-        
-        return [
-            firstName: firstName,
-            lastName: lastName,
-            initials: initials,
-            wordCount: parts.size(),
-            uppercased: fullName.toUpperCase(),
-            reversed: reversed
-        ]
-    }
-]
 
 // Define and return the function in a map
 return [
