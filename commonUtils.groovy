1cec7666486602b6e1a6921d0f05ee3c4bf3b6c1
consolTzotzis <evangelos.tzotzis@consolut.com>
2025-05-30 08:40:28 +0300

    Update commonUtils.groovy

--- commonUtils.groovy
+++ commonUtils.groovy
@@ -1,10 +1,6 @@
 // commonUtils.groovy
 
-def calledFunction1(input) {
-    return " Processed by calledFunction1: ${input}"
-}
-
-// Define and return the function inside a map
+// Define and return the function in a map
 return [
     calledFunction1: { input -> " Processed by calledFunction1: ${input}" }
 ]
